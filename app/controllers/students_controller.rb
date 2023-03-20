cf72d041360c9566c3dce72389d57226d64156e9
zaiky09 <zamilmozamil@outlook.com>
2023-03-20 09:19:52 +0300

    done

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,13 @@
+class StudentsController < ApplicationController
+
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        students = Student.order(grade: :desc)
+        render json: students
+    end
+
+end
